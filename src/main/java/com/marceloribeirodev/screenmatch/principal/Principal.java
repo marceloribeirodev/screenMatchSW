c4c4b563029de9b7b3f3e14392a03e22cd092fc2
unknown <contatoceloribeiro@gmail.com>
2024-11-26 17:32:54 -0300

    Implantação de formatação de datas

--- src/main/java/com/marceloribeirodev/screenmatch/principal/Principal.java
+++ src/main/java/com/marceloribeirodev/screenmatch/principal/Principal.java
@@ -7,6 +7,8 @@ import com.marceloribeirodev.screenmatch.model.Episodio;
 import com.marceloribeirodev.screenmatch.service.ConsumoApi;
 import com.marceloribeirodev.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -69,5 +71,21 @@ public class Principal {
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach( e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episodio: " + e.getTitulo() +
+                                " Data lançamento " + e.getDataLancamento().format(formatador)
+                ));
     }
 }

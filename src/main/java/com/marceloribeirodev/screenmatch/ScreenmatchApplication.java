afd84c2a5e321ec5af4391187f3d3ad8f370f370
unknown <contatoceloribeiro@gmail.com>
2024-11-29 18:11:26 -0300

    CrAtualizacao do application.properties, criaÃcao da nova classe principal

--- src/main/java/com/marceloribeirodev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/marceloribeirodev/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,6 @@
 package com.marceloribeirodev.screenmatch;
 
-import com.marceloribeirodev.screenmatch.principal.Principal;
+import com.marceloribeirodev.screenmatch.principal.NewPrincipal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
-		principal.exibeMenu();
+		NewPrincipal newPrincipal = new NewPrincipal();
+		newPrincipal.exibeMenu();
 	}
 }
